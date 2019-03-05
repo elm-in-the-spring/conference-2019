164a31760ef8aa63d70c0c53f1ef75a892524678
Caroline Artz <ceartz@gmail.com>
2019-03-05 10:14:19 -0600

    footer.

--- src/View/Footer.elm
+++ src/View/Footer.elm
@@ -1,12 +1,34 @@
 module View.Footer exposing (render)
 
 import Dom
+import Html.Attributes as Attr exposing (..)
 import Model exposing (Model)
 import Update exposing (Msg)
 
 
 render : Model -> Dom.Element Msg
 render _ =
-    Dom.element "div"
+    Dom.element "footer"
         |> Dom.addClass "footer"
-        |> Dom.appendText "FOOTER placeholder"
+        |> Dom.appendChild content
+
+
+content =
+    Dom.element "div"
+        |> Dom.addClass "flex justify-around text-blue text-lg text-center"
+        |> Dom.appendChildList
+            [ copyright
+            , github
+            ]
+
+
+copyright =
+    Dom.element "div"
+        |> Dom.appendText "© Elm in the Spring 2019"
+
+
+github =
+    Dom.element "a"
+        |> Dom.addClass "text-blue"
+        |> Dom.addAttribute (href "https://github.com/elm-in-the-spring/conference-2019")
+        |> Dom.appendText "This site is open-source and written with Elm!"
